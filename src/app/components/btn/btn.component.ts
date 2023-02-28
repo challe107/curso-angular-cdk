10e784e872fc143eaca54af2188bdefe6bfc7d12
Carlos Valladares <cvalladares@canvia.com>
2023-02-28 15:13:36 -0500

    Clase 06

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -7,10 +7,25 @@ import { Component, OnInit, Input } from '@angular/core';
 export class BtnComponent implements OnInit {
 
   @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
+  @Input() color = 'success';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  get colors() {
+    return {
+      'bg-success-700': this.color === 'success',
+      'hover:bg-success-800': this.color === 'success',
+      'focus:ring-success-300': this.color === 'success',
+      'bg-primary-700': this.color === 'primary',
+      'hover:bg-primary-800': this.color === 'primary',
+      'focus:ring-primary-300': this.color === 'primary',
+      'bg-danger-700': this.color === 'danger',
+      'hover:bg-danger-800': this.color === 'danger',
+      'focus:ring-danger-300': this.color === 'danger',
+    };
   }
 
 }
