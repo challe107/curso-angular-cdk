24fcc9a0803f3b6b01d810e27970063de11054b4
Carlos Valladares <cvalladares@canvia.com>
2023-03-01 01:16:30 -0500

    Clase 23 y Clase 24

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -7,7 +7,40 @@ import { Component, OnInit, Input } from '@angular/core';
 export class BtnComponent implements OnInit {
 
   @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
-  @Input() color = 'success';
+  @Input() color: 'success' | 'primary' | 'danger' | 'light' | 'sky' = 'success';
+
+  mapColors = {
+    success: {
+      'bg-success-700': true,
+      'hover:bg-success-800': true,
+      'focus:ring-success-300': true,
+      'text-white': true,
+    },
+    primary: {
+      'bg-primary-700': true,
+      'hover:bg-primary-800': true,
+      'focus:ring-primary-300': true,
+      'text-white': true,
+    },
+    danger: {
+      'bg-danger-700': true,
+      'hover:bg-danger-800': true,
+      'focus:ring-danger-300': true,
+      'text-white': true,
+    },
+    light: {
+      'bg-gray-200': true,
+      'hover:bg-gray-500': true,
+      'focus:ring-gray-50': true,
+      'text-gray-700': true,
+    },
+    sky: {
+      'bg-sky-700': true,
+      'hover:bg-sky-800': true,
+      'focus:ring-sky-300': true,
+      'text-white': true,
+    }
+  }
 
   constructor() { }
 
@@ -15,22 +48,13 @@ export class BtnComponent implements OnInit {
   }
 
   get colors() {
-    return {
-      'text-white': this.color === 'success' || this.color === 'primary' || this.color === 'danger',
-      'text-gray-700': this.color === 'gray-light',
-      'bg-success-700': this.color === 'success',
-      'hover:bg-success-800': this.color === 'success',
-      'focus:ring-success-300': this.color === 'success',
-      'bg-primary-700': this.color === 'primary',
-      'hover:bg-primary-800': this.color === 'primary',
-      'focus:ring-primary-300': this.color === 'primary',
-      'bg-danger-700': this.color === 'danger',
-      'hover:bg-danger-800': this.color === 'danger',
-      'focus:ring-danger-300': this.color === 'danger',
-      'bg-gray-200': this.color === 'gray-light',
-      'hover:bg-gray-500': this.color === 'gray-light',
-      'focus:ring-gray-50': this.color === 'gray-light',
-    };
+    const colors = this.mapColors[this.color];
+
+    if (colors) {
+      return colors;
+    }
+
+    return {};
   }
 
 }
