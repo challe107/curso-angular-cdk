be5cf11d35bc03d690eabd3d83a24a606ac38eeb
Carlos Valladares <cvalladares@canvia.com>
2023-02-28 23:59:06 -0500

    Clase 20

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -9,6 +9,7 @@ import { Product } from 'src/app/models/product.model';
 export class TableComponent implements OnInit {
   products: Product[] = [];
   columns: string[] = ['id', 'title','price', 'cover'];
+  total = 0;
 
   constructor(
     private http: HttpClient
@@ -17,6 +18,7 @@ export class TableComponent implements OnInit {
   ngOnInit(): void {
     this.http.get<Product[]>(`https://api.escuelajs.co/api/v1/products`).subscribe((data) => {
       this.products = data;
+      this.total = this.products.map((item) => item.price).reduce((price, total) => price + total, 0);
     });
   }
 
